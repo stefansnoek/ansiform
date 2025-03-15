e80cc266fb50e7a4de197d2f01fb37a089094e92
stefansnoek <60693056+stefansnoek@users.noreply.github.com>
2025-03-15 13:18:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-    features {}
-    subscription_id = "3087dfe8-7fda-4ff3-adcf-86ab5103796b"
-}
-
 resource "azurerm_resource_group" "rg" {
     name     = "rg-ansiform"
     location = "North Europe"
